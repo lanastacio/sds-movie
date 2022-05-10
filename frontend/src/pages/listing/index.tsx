f60f3a55a607d0380ae7718e64d1fc5eb3afecef
lanastacio <lucas_anastacio@hotmail.com>
2022-05-10 20:53:47 -0300

    Pagination Layout

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,8 +1,10 @@
+import Pagination from "components/pagination";
+
 function Listing(){
 
     return(
-        <h1>Página de Listagem</h1>
-    )
+        <Pagination />
+    );
 }
 
 export default Listing;
